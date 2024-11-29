817c3e30fdf178c00fa7a163c0aa417dfa611338
chaunhattang <tang.chaunhat.cit22@eiu.edu.vn>
2024-11-29 17:30:49 +0700

    update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("CSE 107");
+        System.out.println("Hello ChauNhatTang");
+        System.out.println("Welcome to my repository");
     }
 }
